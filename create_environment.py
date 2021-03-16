01fb94988a56696bd216fb225b4c99c0ed218af9
kartikbatra056 <43983856+kartikbatra056@users.noreply.github.com>
2021-03-16 13:24:40 +0530

    Add files via upload

--- create_environment.py
+++ create_environment.py
@@ -1,15 +1,34 @@
 # Import Libraries
-from azureml.core import Environment,Workspace
+from azureml.core.environment import Environment
+from azureml.core.conda_dependencies import CondaDependencies
 
 # get workspace
 ws = Workspace.from_config()
+
+
+# upload locally created wheel file to azure environment
+whl_url = Environment.add_private_pip_wheel(workspace=ws,file_path ="src/dist/Myestimator-0.0.1-py3-none-any.whl")
+
+# create CondaDependencies
+
+myenv = CondaDependencies()
+myenv.add_pip_package("scikit-learn")
+myenv.add_pip_package("numpy")
+myenv.add_pip_package("pandas")
+myenv.add_pip_package("seaborn")
+myenv.add_pip_package("matplotlib")
+myenv.add_pip_package("azureml-defaults")
+myenv.add_pip_package(whl_url)
+
+# write dependencies to a yaml file
+with open("./.azureml/python-env.yml","w") as f:
+    f.write(myenv.serialize_to_string())
 
 # Create environment
 env = Environment.from_conda_specification(
         name='python-env',
         file_path='./.azureml/python-env.yml'
     )
-
 
 # register environment
 env.register(workspace=ws)
